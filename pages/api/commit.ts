936add7aa5bdbdcfc2ff4d519340f8ee9a499239
Peter Skaltsis <peterjskaltsis@gmail.com>
2023-02-14 11:45:09 +1100

    fix: git api

--- pages/api/commit.ts
+++ pages/api/commit.ts
@@ -1,21 +1,33 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import simpleGit, { DefaultLogFields, LogResult } from "simple-git";
 
-type Data = Partial<DefaultLogFields> | null;
+type Data = { date: string; hash: string } | null;
 
-const git = simpleGit();
+async function getLatestCommitData(owner: string, repo: string) {
+  const response = await fetch(
+    `https://api.github.com/repos/${owner}/${repo}/commits`
+  );
+  const data = await response.json();
+
+  if (data && data.length > 0) {
+    const latestCommit = data[0];
+    const latestCommitDate = latestCommit.commit.committer.date;
+    const latestCommitHash = latestCommit.sha;
+
+    return { date: latestCommitDate, hash: latestCommitHash };
+  }
+
+  return null;
+}
 
 export default async function handler(
   _req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const log = await git.log();
-  const lastCommit = log.latest;
+  const commit = await getLatestCommitData("peterjskaltsis", "site");
 
   res.status(200).json({
-    date: lastCommit?.date,
-    message: lastCommit?.message,
-    hash: lastCommit?.hash,
+    date: commit?.date,
+    hash: commit?.hash,
   });
 }
